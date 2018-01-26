a3b68b2c33ab4ab2caefe9991577a6763653ccc0
vivekponnaiya <35727147+vivekponnaiya@users.noreply.github.com>
2018-01-26 11:05:23 +0530

    Create vivek odd or even.c

---
+++ vivek odd or even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int p;
+clrscr();
+printf("Enter a number to find whether it is odd or even");
+scanf("%d",&p);
+if(p%2==0)
+{
+printf("Even");
+}
+else
+{
+printf("Odd");
+}
+}
